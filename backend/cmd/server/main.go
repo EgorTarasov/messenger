15d2632f531f0a3bdd8796d33c5faa2a2df130b4
EgorTarasov <61450170+EgorTarasov@users.noreply.github.com>
2025-07-05 19:00:55 +0300

    [build]: added default user and github action to deploy application

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -2,17 +2,24 @@ package main
 
 import (
 	"log"
+	_ "messenger/migrations"
 	"messenger/static"
 
 	"github.com/pocketbase/pocketbase"
 	"github.com/pocketbase/pocketbase/apis"
 	"github.com/pocketbase/pocketbase/core"
+	"github.com/pocketbase/pocketbase/plugins/migratecmd"
 )
 
 
 func main() {
     app := pocketbase.New()
     static.Init()
+
+    migratecmd.MustRegister(app, app.RootCmd, migratecmd.Config{
+        Automigrate: true,
+    })
+
     // react application route
     app.OnServe().BindFunc(func(se *core.ServeEvent) error {
         se.Router.GET("/{path...}", apis.Static(static.ReactFS, true))
