51583e95756efa62b3f3b2c7617dc0c4659d2f4c
EgorTarasov <61450170+EgorTarasov@users.noreply.github.com>
2025-07-06 00:06:42 +0300

    [fix]: removed build env variable for api endpoint

--- ui/src/shared/pocketbase/index.ts
+++ ui/src/shared/pocketbase/index.ts
@@ -1,8 +1,8 @@
 import PocketBase from "pocketbase";
 
-const pb = new PocketBase(
-  import.meta.env.VITE_POCKETBASE_URL || "http://127.0.0.1:8090",
-);
+const baseUrl = import.meta.env.DEV ? "http://127.0.0.1:8090" : "/";
+
+const pb = new PocketBase(baseUrl);
 
 pb.autoCancellation(false);
 export default pb;
